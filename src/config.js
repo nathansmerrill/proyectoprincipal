4d7a802e2f5fa4cccc70fae678188a31efb70b19
Nathan Merrill <mathiscool3000@gmail.com>
2020-05-27 20:31:56 +0000

    Implement modular room design

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    'start': {
+        text: 'hi this is the starting room. Do you want to <b>stuff</b> or <b>second stuff</b>',
+        options: {
+            'stuff': 'armorRoom',
+            'second stuff': 'room2'
+        }
+    },
+    'armorRoom': {
+        text: 'This is the armor room',
+        options: {}
+    },
+    'room2': {
+        text: 'This is the second room',
+        options: {}
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('Hi World');
+import $ from 'jquery';
+
+import {keyNames} from "./common";
+import config from './config';
+import './main.scss';
+
+const output = $('#output');
+const input = $('#input');
+
+let currentRoom = 'start';
+
+function outputAppend(text) {
+    output.append(`<span class="output-line">${text}</span>`);
+    $(window).scrollTop($('body').height());
+}
+
+function printRoomText() {
+    outputAppend(config[currentRoom].text);
+}
+
+printRoomText();
+
+input.on('blur',function () {
+    let blurEl = $(this);
+    setTimeout(function() {
+        blurEl.focus();
+    }, 10);
+});
+
+$(document).keypress(e => {
+    let key = keyNames[e.charCode];
+    if (key === 'enter') {
+        if (config[currentRoom].options[input.val()] === undefined) {
+            // outputAppend(`Invalid option. The valid options are ${Object.keys(config[currentRoom].options)}`);
+            outputAppend(`Invalid option. The valid options are ${Object.keys(config[currentRoom].options).map(v => ' <b>' + v + '</b>')}`);
+        } else {
+            currentRoom = config[currentRoom].options[input.val()];
+            input.val('');
+            printRoomText();
+        }
+    }
+});
